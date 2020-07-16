9cdf8bd6b083752dc6be9332920545b35aaf8009
wanglei <765205025@qq.com>
2020-07-16 14:52:33 +0800

    超时自动出牌

--- assets/Script/Component/GameSchedule.ts
+++ assets/Script/Component/GameSchedule.ts
@@ -26,7 +26,7 @@ export default class GameSchedule extends cc.Component {
 
     executed: boolean = true; //是否已执行完毕
 
-    delta: number = 0;
+    // delta: number = 0;
 
     player: GameFramePlayer = null;
 
@@ -41,19 +41,17 @@ export default class GameSchedule extends cc.Component {
 
     }
 
-    update(delta: number) {
-        this.delta += delta;
-        if (this.delta > 0.5) {
-            this.delta -= 0.5;
-            if (!this.executed) {
-                const d = this.deltaTime + new Date().getTime() - this.actionTime;
-                if (d >= this.timeout) {//满足执行条件
-                    this.game.time = 0;
-                    this.task();
-                    this.executed = true;
-                } else {
-                    this.game.time = parseInt(((this.timeout - d) / 1000).toFixed(0));
-                }
+    updateBySchedule() {
+        console.log("updateBySchedule")
+        if (!this.executed) {
+            const d = this.deltaTime + new Date().getTime() - this.actionTime;
+            if (d >= this.timeout) {//满足执行条件
+                this.game.time = 0;
+                this.task();
+                this.executed = true;
+                this.unscheduleAllCallbacks();
+            } else {
+                this.game.time = parseInt(((this.timeout - d) / 1000).toFixed(0));
             }
         }
     }
@@ -65,15 +63,28 @@ export default class GameSchedule extends cc.Component {
         this.task = task;
         this.executed = false;
         this.game.time = parseInt(((this.timeout) / 1000).toFixed(0));
+
+        const delta = this.getRestTime();
+        
+        if (delta > 0) {
+            const times = Math.max(parseInt((delta / 1000).toFixed(0)), 2);
+            console.log(times);
+            this.unscheduleAllCallbacks();
+            this.schedule(this.updateBySchedule, 1, times);
+        }
+    }
+
+    private getRestTime(): number {
+        return this.timeout - this.deltaTime + new Date().getTime() - this.actionTime;
     }
 
     changeOrder(newTurn: number, oldTurn: number) {
         const turn = newTurn;
         this.addTask(this.player.time, this.player.serverTime, this.player.clientTime, 20000, () => {
             const action = this.game.botAction(turn);
-            // if (action) {
-            //     this.player.sendAction(action);
-            // }
+            if (action) {
+                this.player.sendAction(action);
+            }
         });
     }
 }
